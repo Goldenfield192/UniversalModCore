4f5b82b1738a139d7d3ffca04e1236feb94baf4a
Goldenfield192 <1437356849@qq.com>
2025-04-17 16:48:49 +0800

    1.17 backup

--- src/main/java/cam72cam/mod/block/tile/TileEntity.java
+++ src/main/java/cam72cam/mod/block/tile/TileEntity.java
@@ -210,6 +210,9 @@ public class TileEntity extends net.minecraft.world.level.block.entity.BlockEnti
         super.load(compound);
         hasTileData = true;
 
+        // Hack; Because of the new BlockEntity Constructor we now have to set the position manually after loading the BE
+        setPos(new BlockPos(compound.getInt("x"), compound.getInt("y"), compound.getInt("z")));
+
         TagCompound data = new TagCompound(compound);
         TagCompound instanceData = data.get("instanceData");
         if (instanceData == null) {
