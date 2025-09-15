b0e3b053e86ee614306654fe86cb18faaa122f5c
Goldenfield192 <1437356849@qq.com>
2025-09-15 19:15:09 +0800

    fix chinese char display bug

--- src/main/java/cam72cam/mod/ModCore.java
+++ src/main/java/cam72cam/mod/ModCore.java
@@ -1,7 +1,7 @@
 package cam72cam.mod;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
@@ -344,7 +344,7 @@ public class ModCore {
                             translations.add(String.format("\"%s\": \"%s\"", key.replace(".name", "").replace(":", "."), value));
                         });
                         String output = "{" + String.join(",", translations) + "}";
-                        return new ByteArrayInputStream(output.getBytes(StandardCharsets.UTF_8));
+                        return new ByteArrayInputStream(output.getBytes(Charset.defaultCharset()));
                     }
                 }
                 return null;
