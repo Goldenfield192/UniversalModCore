0cdc732e76b2e2d86feac35e0a381c79858b7bb6
Goldenfield192 <1437356849@qq.com>
2025-11-16 20:38:25 +0800

    fix: update template file

--- src/main/resources/template/src/main/java/PACKAGEPATH/Mod.java
+++ src/main/resources/template/src/main/java/PACKAGEPATH/Mod.java
@@ -4,8 +4,8 @@ import cam72cam.mod.ModCore;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.data.event.GatherDataEvent;
 
-@net.minecraftforge.fml.common.Mod(Mod.MODID)
-@net.minecraftforge.fml.common.Mod.EventBusSubscriber(modid = Mod.MODID, bus = net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus.MOD)
+@net.neoforged.fml.common.Mod(Mod.MODID)
+@net.neoforged.fml.common.Mod.EventBusSubscriber(modid = Mod.MODID, bus = net.neoforged.fml.common.Mod.EventBusSubscriber.Bus.MOD)
 public class Mod {
     public static final String MODID = "#ID#";
 
