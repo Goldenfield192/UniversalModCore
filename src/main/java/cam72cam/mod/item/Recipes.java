4e66d7566f267884d3744a678809fa61c6e942b2
Goldenfield192 <1437356849@qq.com>
2026-02-13 17:23:42 +0800

    feat: shapeless recipes

--- src/main/java/cam72cam/mod/item/Recipes.java
+++ src/main/java/cam72cam/mod/item/Recipes.java
@@ -9,6 +9,7 @@ import net.minecraftforge.common.crafting.CraftingHelper;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.oredict.OreIngredient;
 import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,32 +17,58 @@ import java.util.List;
 
 /** Recipe registration */
 public class Recipes {
-
-    public static ShapedRecipeBuilder shapedRecipe(CustomItem item, int width, Fuzzy... ingredients) {
+    public static RecipeBuilder shapedRecipe(CustomItem item, int width, Fuzzy... ingredients) {
         return new ShapedRecipeBuilder(new ItemStack(item, 1), width, ingredients);
     }
 
-    public static ShapedRecipeBuilder shapedRecipe(ItemStack item, int width, Fuzzy... ingredients) {
+    public static RecipeBuilder shapedRecipe(ItemStack item, int width, Fuzzy... ingredients) {
         return new ShapedRecipeBuilder(item, width, ingredients);
     }
 
-    public static class ShapedRecipeBuilder {
+    public static RecipeBuilder shapelessRecipe(CustomItem item, Fuzzy... ingredients) {
+        return new ShapelessRecipeBuilder(new ItemStack(item, 1), ingredients);
+    }
+
+    public static RecipeBuilder shapelessRecipe(ItemStack item, Fuzzy... ingredients) {
+        return new ShapelessRecipeBuilder(item, ingredients);
+    }
+
+    public static abstract class RecipeBuilder {
         private List<Fuzzy> dependencies = new ArrayList<>();
         private List<Fuzzy> conflicts = new ArrayList<>();
 
+        public boolean checkCondition() {
+            for (Fuzzy dependency : dependencies) {
+                if (dependency.enumerate().isEmpty()) {
+                    // Don't register recipe
+                    return false;
+                }
+            }
+            for (Fuzzy conflict : conflicts) {
+                if (!conflict.enumerate().isEmpty()) {
+                    // Don't register recipe
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public RecipeBuilder require(Fuzzy ...dependencies) {
+            this.dependencies.addAll(Arrays.asList(dependencies));
+            return this;
+        }
+
+        public RecipeBuilder conflicts(Fuzzy ...conflicts) {
+            this.conflicts.addAll(Arrays.asList(conflicts));
+            return this;
+        }
+    }
+
+    public static class ShapedRecipeBuilder extends RecipeBuilder{
         private ShapedRecipeBuilder(ItemStack item, int width, Fuzzy... ingredients) {
             CommonEvents.Recipe.REGISTER.subscribe(() -> {
-                for (Fuzzy dependency : dependencies) {
-                    if (dependency.enumerate().isEmpty()) {
-                        // Don't register recipe
-                        return;
-                    }
-                }
-                for (Fuzzy conflict : conflicts) {
-                    if (!conflict.enumerate().isEmpty()) {
-                        // Don't register recipe
-                        return;
-                    }
+                if (!checkCondition()) {
+                    return;
                 }
 
                 CraftingHelper.ShapedPrimer primer = new CraftingHelper.ShapedPrimer();
@@ -60,15 +87,24 @@ public class Recipes {
                 ForgeRegistries.RECIPES.register(sor);
             });
         }
+    }
 
-        public ShapedRecipeBuilder require(Fuzzy ...dependencies) {
-            this.dependencies.addAll(Arrays.asList(dependencies));
-            return this;
-        }
+    public static class ShapelessRecipeBuilder extends RecipeBuilder{
+        private ShapelessRecipeBuilder(ItemStack item, Fuzzy... ingredients) {
+            CommonEvents.Recipe.REGISTER.subscribe(() -> {
+                CraftingHelper.ShapedPrimer primer = new CraftingHelper.ShapedPrimer();
+                primer.mirrored = false;
+                primer.input = NonNullList.withSize(primer.width * primer.height, Ingredient.EMPTY);
 
-        public ShapedRecipeBuilder conflicts(Fuzzy ...conflicts) {
-            this.conflicts.addAll(Arrays.asList(conflicts));
-            return this;
+                for (int i = 0; i < ingredients.length; i++) {
+                    if (ingredients[i] != null) {
+                        primer.input.set(i, new OreIngredient(ingredients[i].toString()));
+                    }
+                }
+                ShapelessOreRecipe sor = new ShapelessOreRecipe(new ResourceLocation(ModCore.MODID, "recipes"), item.internal, primer);
+                sor.setRegistryName(item.internal.getItem().getRegistryName());
+                ForgeRegistries.RECIPES.register(sor);
+            });
         }
     }
 }
