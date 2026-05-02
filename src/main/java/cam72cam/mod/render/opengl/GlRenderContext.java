1da88940327ef9030b46768b2b560c8934f85d08
Goldenfield192 <1437356849@qq.com>
2026-05-02 10:54:50 +0800

    ref: use instance fields

--- src/main/java/cam72cam/mod/render/opengl/GlRenderContext.java
+++ src/main/java/cam72cam/mod/render/opengl/GlRenderContext.java
@@ -6,8 +6,9 @@ import cam72cam.mod.render.api.RenderCtx;
 import cam72cam.mod.util.With;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
-import net.minecraft.client.renderer.GLAllocation;
 import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.relauncher.Side;
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
@@ -20,9 +21,14 @@ import java.util.*;
 import static cam72cam.mod.render.opengl.Texture.NO_TEXTURE;
 
 public class GlRenderContext extends RenderCtx {
-    private static FloatBuffer fourFloatBuffer;
+    private FloatBuffer colorObj;
+    private FloatBuffer matrixObj = null;
 
     public GlRenderContext() {
+        if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
+            colorObj = BufferUtils.createFloatBuffer(16);
+            matrixObj = BufferUtils.createFloatBuffer(16);
+        }
     }
 
     public With apply(RenderState state) {
@@ -168,12 +174,8 @@ public class GlRenderContext extends RenderCtx {
         if (state.color != null) {
             boolean oldColorMaterial = GL11.glGetBoolean(GL11.GL_COLOR_MATERIAL);
             applyBool(GL11.GL_COLOR_MATERIAL, true);
-
-            if (fourFloatBuffer == null) {
-                fourFloatBuffer = GLAllocation.createDirectFloatBuffer(16);
-            }
-            GL11.glGetFloat(GL11.GL_CURRENT_COLOR, fourFloatBuffer);
-            float[] oldColor = new float[] {fourFloatBuffer.get(0), fourFloatBuffer.get(1), fourFloatBuffer.get(2), fourFloatBuffer.get(3)};
+            GL11.glGetFloat(GL11.GL_CURRENT_COLOR, colorObj);
+            float[] oldColor = new float[] {colorObj.get(0), colorObj.get(1), colorObj.get(2), colorObj.get(3)};
             GL11.glColor4f(state.color[0], state.color[1], state.color[2], state.color[3]);
             restore.add(() -> {
                 GL11.glColor4f(oldColor[0], oldColor[1], oldColor[2], oldColor[3]);
@@ -249,21 +251,16 @@ public class GlRenderContext extends RenderCtx {
         return () -> restore.forEach(Runnable::run);
     }
 
-    private static FloatBuffer fbm = null;
-    private static void multMatrix(Matrix4 matrix) {
-        if (fbm == null) {
-            // Can't static init since class is loaded server side
-            fbm = BufferUtils.createFloatBuffer(16);
-        }
-        fbm.position(0);
-        fbm.put(new float[]{
+    private void multMatrix(Matrix4 matrix) {
+        matrixObj.position(0);
+        matrixObj.put(new float[]{
                 (float) matrix.m00, (float) matrix.m01, (float) matrix.m02, (float) matrix.m03,
                 (float) matrix.m10, (float) matrix.m11, (float) matrix.m12, (float) matrix.m13,
                 (float) matrix.m20, (float) matrix.m21, (float) matrix.m22, (float) matrix.m23,
                 (float) matrix.m30, (float) matrix.m31, (float) matrix.m32, (float) matrix.m33
         });
-        fbm.flip();
-        GL11.glMultMatrix(fbm);
+        matrixObj.flip();
+        GL11.glMultMatrix(matrixObj);
     }
 
     static void applyBool(int glOptCode, boolean state) {
